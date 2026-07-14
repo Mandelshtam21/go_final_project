f2c6f43038dca71f64e1cf60fc1eaeeb0f33872e
Kim <kim@practicum.kz>
2026-07-14 18:51:54 +0500

    Update and improve api.go, addtask.go, and task.go

--- pkg/api/addtask.go
+++ pkg/api/addtask.go
@@ -1,1 +1,104 @@
 package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"go_final_project/pkg/db"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func addTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task db.Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		writeJson(w, map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = json.Unmarshal(buf.Bytes(), &task)
+	if err != nil {
+		writeJson(w, map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if task.Title == "" {
+		writeJson(w, map[string]string{
+			"error": "Не указан заголовок задачи",
+		})
+		return
+	}
+
+	err = checkDate(&task)
+	if err != nil {
+		writeJson(w, map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	id, err := db.AddTask(&task)
+	if err != nil {
+		writeJson(w, map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+	idStr := strconv.Itoa(int(id))
+	writeJson(w, map[string]string{
+		"id": idStr,
+	})
+}
+
+func checkDate(task *db.Task) error {
+	var next string
+	now := time.Now()
+	now, err := time.Parse(DateFormat, now.Format(DateFormat))
+	if err != nil {
+		return err
+	}
+
+	if task.Date == "" {
+		task.Date = now.Format(DateFormat)
+	}
+
+	t, err := time.Parse(DateFormat, task.Date)
+	if err != nil {
+		return err
+	}
+
+	if task.Repeat != "" {
+		next, err = NextDate(now, task.Date, task.Repeat)
+		if err != nil {
+			return err
+		}
+	}
+
+	if afterNow(now, t) {
+		if len(task.Repeat) == 0 {
+			task.Date = now.Format(DateFormat)
+		} else {
+			task.Date = next
+		}
+	}
+
+	return nil
+}
+
+func writeJson(w http.ResponseWriter, data any) {
+	resp, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(resp)
+}
--- pkg/db/task.go
+++ pkg/db/task.go
@@ -1,5 +1,9 @@
 package db
 
+import (
+	"database/sql"
+)
+
 type Task struct {
 	ID      string `json:"id"`
 	Date    string `json:"date"`
@@ -11,9 +15,14 @@ type Task struct {
 func AddTask(task *Task) (int64, error) {
 	var id int64
 
-	query := `INSERT INTO scheduler `
-	res, err := db.Exec(query)
-	if err != nil {
+	query := `INSERT INTO scheduler (date, title, comment, repeat) 
+	VALUES (:date, :title, :comment, :repeat)`
+	res, err := db.Exec(query,
+		sql.Named("date", task.Date),
+		sql.Named("title", task.Title),
+		sql.Named("comment", task.Comment),
+		sql.Named("repeat", task.Repeat))
+	if err == nil {
 		id, err = res.LastInsertId()
 	}
 	return id, err
